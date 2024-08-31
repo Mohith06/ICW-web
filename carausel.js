8ba130edf3982c94f67fab026e3ad33c5bb9f6eb
Mohith06 <80212547+Mohith06@users.noreply.github.com>
2024-08-31 14:58:06 -0500

    Delete carausel.js

--- carausel.js
+++
@@ -1,9 +0,0 @@
-// Manages carousel functionality.
-document.addEventListener('DOMContentLoaded', function () {
-    // Initialize Bootstrap Carousel
-    var myCarousel = document.querySelector('#newsCarousel');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 5000, // Time interval for sliding (in milliseconds)
-        wrap: true
-    });
-});
